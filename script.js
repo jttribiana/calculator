39ca65721601b07f72de211e3e00b74bd60ee7cd
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-01-24 19:56:42 +0800

    Populate display to the input box

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-
+/* calculator operations */
 
 function add (a, b){
     return a + b;
@@ -19,66 +19,48 @@ function divide (a, b){
     return a / b;
 }
 
-/*
-
-1. make a function that takes values of an operator, and two numbers like 3 + 5
-2. make the function call to one of the operations above. 
-
-
-create a function that would take parameters like two numbers and one operator
-create variable for these three things
-
-*/
-
 
 function operator (){
-    let operation = '+';
+    let operation = '-';
     let a = 1;
     let b = 2;
 
     if (operation === '+'){
         return add (a, b);
+    } else if (operation === '-'){
+        return subtract (a, b);
+    } else if (operation === '*'){
+        return multiply (a, b);
+    } else if (operation === '/'){
+        return divide (a, b);
     }
 }
 operator();
 console.log(operator())
 
-/*
+
+
+
+//make the number populate to the display
 
 function display (){
 
     const input = document.getElementById('input');
     const num = document.getElementsByClassName ('num');
+    
 
-    for( var i=0; i<num.length; i++){
+    for( var i=0; i<num.length; i++){ //using for loop to listen to multiple class button click
         num[i].addEventListener ('click', function(e){
-        return num1 = input.value = (e.target.value);     
+        let number = (e.target.value);
+        input.value += number; //populate the number in the display
     })
     }
 }
 display ();
 
 
-*/
 
 
 
 
-
-//operator functions
-
-
-
-
-//for computation and display
-
-
-
-//operations making operations work
-
-
-
-
-
-///test
 
