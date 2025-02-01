316b2ab461f056ae0df460512be7a2cb2702ede5
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-02-01 19:10:24 +0800

    Fix ui

--- script.js
+++ script.js
@@ -19,8 +19,46 @@ function divide (a, b){
 
 
 
-/* this is the variables for operations */
+let operation = '/';
+let firstNum = '1';
+let secondNum = '2';
 
+function operate (){  
+    if (operation === '+'){
+        return add (firstNum, secondNum);
+    } else if (operation === '-'){
+        return subtract (firstNum, secondNum);
+    } else if (operation === '*'){
+        return multiply (firstNum, secondNum);
+    } else if (operation === '/'){
+        return divide (firstNum, secondNum);
+    } 
+}
+operate ();
+console.log(operate());
+
+
+
+
+/* this function listens to operation clicked and operate */
+
+//have to fix the function operate logic wherein it should get three values
+/*
+const operators = document.getElementsByClassName('operator');
+
+for( var i=0; i<operators.length; i++){ //using for loop to listen to multiple class button click
+    operators[i].addEventListener ('click', function(e){
+    operation = (e.target.value);
+    console.log(operation);
+})
+}
+
+    //operation logic
+
+
+
+/* this function displays the number in the input box */
+/*
 function display (){
     const input = document.getElementById('input');
     const num = document.getElementsByClassName('btn');
@@ -28,63 +66,21 @@ function display (){
 
     for( var i=0; i<num.length; i++){ //using for loop to listen to multiple class button click
         num[i].addEventListener ('click', function(e){
-        let number = (e.target.value);
-        let numberValue = input.value += number; //populate the number in the display
-        
-        
-        let a = numberValue; // storing in the value to a variable, but populating weirdly in console
-        console.log(a)
-
-
-        let b = 2;
-
-
-//variables & buttons
-
-const operators = document.getElementsByClassName('operator');
-const equalBtn = document.getElementById ('equalBtn');
-
-for( var i=0; i<operators.length; i++){ //using for loop to listen to multiple class button click
-    operators[i].addEventListener ('click', function(e){
-    operation = (e.target.value);
-    console.log(operation);
-
-    //operation logic
-    function operate (){
-
-    
-        if (operation === '+'){
-            return add (a, b);
-        } else if (operation === '-'){
-            return subtract (a, b);
-        } else if (operation === '*'){
-            return multiply (a, b);
-        } else if (operation === '/'){
-            return divide (a, b);
-        } 
-    
-    }
-    operate ();
-    console.log(operate());
-    
-
-})
-}
+        number = input.value += (e.target.value); //populate the number in the display
 })
 }
 }
 display ();
-
-
-
-
-//make the number populate to the display
-
-
-
-
-
-
-
-
-
+*/
+//sample code, event listener with variable
+/*
+document.querySelector('#buttons')
+  .addEventListener('click', event => {
+    let target = event.target;
+    if (target.matches('button')) {
+      let value = target.innerHTML
+      
+      document.querySelector('#output').value += value
+    }
+  });
+*/
