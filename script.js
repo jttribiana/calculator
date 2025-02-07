7b931eeace95e0a42915c37ebfcbd1f08a46e1bd
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-02-07 20:53:32 +0800

    Make operations work

--- script.js
+++ script.js
@@ -1,86 +1,61 @@
 
-/* calculator operations */
 
-function add (a, b){
-    return a + b;
-}
 
-function subtract (a, b) {
-    return a - b;
-}
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 
-function multiply (a, b){
-    return a * b;
-}
 
-function divide (a, b){
-    return a / b;
-}
+const number = document.querySelectorAll ('.btn.num');
+const operators = document.querySelectorAll ('.btn.operator');
+const inputField = document.getElementById ('input');
 
 
 
-let operation = '/';
-let firstNum = '1';
-let secondNum = '2';
-
-function operate (){  
-    if (operation === '+'){
-        return add (firstNum, secondNum);
-    } else if (operation === '-'){
-        return subtract (firstNum, secondNum);
-    } else if (operation === '*'){
-        return multiply (firstNum, secondNum);
-    } else if (operation === '/'){
-        return divide (firstNum, secondNum);
-    } 
-}
-operate ();
-console.log(operate());
+number.forEach (number => {
+    number.addEventListener('click', e => {
+        let innerText = e.target.innerText;
+        if (operator === ''){
+            inputField.value = firstNum += innerText;
+            console.log(`firstNum: ${firstNum}`);
+        } else {
+            inputField.value = secondNum += innerText;
+        }
+    })
+});
 
 
+operators.forEach(op => {
+    op.addEventListener('click', e => {
+        if (e.target.innerText !== '='){
+            operator = e.target.innerText;
+            console.log(`firstNum: ${firstNum}`)
+            console.log(`operator: ${operator}`)
+
+        } else {
+            console.log(`secondNum: ${secondNum}`);
+
+            switch (operator){
+                case '+':
+                    console.log(parseInt(firstNum)+ parseInt (secondNum));
+                    break;
+                case '-':
+                    console.log(parseInt(firstNum)- parseInt (secondNum));
+                    break;        
+                case '*':
+                    console.log(parseInt(firstNum)* parseInt (secondNum));
+                    break;
+                case '/':
+                    console.log(parseInt(firstNum)/ parseInt (secondNum));
+                    break;
+            }
+        }
+    });
+});
 
 
-/* this function listens to operation clicked and operate */
-
-//have to fix the function operate logic wherein it should get three values
-/*
-const operators = document.getElementsByClassName('operator');
-
-for( var i=0; i<operators.length; i++){ //using for loop to listen to multiple class button click
-    operators[i].addEventListener ('click', function(e){
-    operation = (e.target.value);
-    console.log(operation);
-})
-}
-
-    //operation logic
 
 
 
-/* this function displays the number in the input box */
-/*
-function display (){
-    const input = document.getElementById('input');
-    const num = document.getElementsByClassName('btn');
-    
 
-    for( var i=0; i<num.length; i++){ //using for loop to listen to multiple class button click
-        num[i].addEventListener ('click', function(e){
-        number = input.value += (e.target.value); //populate the number in the display
-})
-}
-}
-display ();
-*/
-//sample code, event listener with variable
-/*
-document.querySelector('#buttons')
-  .addEventListener('click', event => {
-    let target = event.target;
-    if (target.matches('button')) {
-      let value = target.innerHTML
-      
-      document.querySelector('#output').value += value
-    }
-  });
-*/
+
